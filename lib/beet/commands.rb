08b227d958331df856dab1244ac9988e79d55894
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-19 17:24:54 -0400

    split out class that runs things into executor

--- lib/beet/commands.rb
+++ lib/beet/commands.rb
@@ -6,29 +6,7 @@ require 'open-uri'
 require 'fileutils'
 
 module Beet
-  class Executor
-    attr_reader :root
-    attr_writer :logger
-
-    def initialize(template, root = '') # :nodoc:
-      @root = File.expand_path(File.directory?(root) ? root : File.join(Dir.pwd, root))
-
-      log 'applying', "template: #{template}"
-
-      load_template(template)
-
-      log 'applied', "#{template}"
-    end
-
-    def load_template(template)
-      begin
-        code = open(template).read
-        in_root { self.instance_eval(code) }
-      rescue LoadError, Errno::ENOENT => e
-        raise "The template [#{template}] could not be loaded. Error: #{e}"
-      end
-    end
-
+  module Commands
     # Create a new file in the project folder.  Specify the
     # relative path from the project's root.  Data is the return value of a block
     # or a data string.
---
+++ lib/beet/executor.rb
@@ -0,0 +1,26 @@
+module Beet
+  class Executor
+    attr_reader :root
+    attr_writer :logger
+
+    def initialize(template, root = '') # :nodoc:
+      @root = File.expand_path(File.directory?(root) ? root : File.join(Dir.pwd, root))
+
+      log 'applying', "template: #{template}"
+
+      load_template(template)
+
+      log 'applied', "#{template}"
+    end
+
+    def load_template(template)
+      begin
+        code = open(template).read
+        in_root { self.instance_eval(code) }
+      rescue LoadError, Errno::ENOENT => e
+        raise "The template [#{template}] could not be loaded. Error: #{e}"
+      end
+    end
+
+  end
+end
