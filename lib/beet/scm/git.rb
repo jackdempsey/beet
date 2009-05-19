5d1605113d6c456fb4c01f0e7eda2a50c2fdc91a
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-19 16:30:21 -0400

    add in scm files

--- lib/beet/scm/git.rb
+++ lib/beet/scm/git.rb
@@ -1,16 +1,36 @@
-module Rails
-  class Git < Scm
-    def self.clone(repos, branch=nil)
-      `git clone #{repos}`
+class Git < Scm
+  def self.clone(repos, branch=nil)
+    `git clone #{repos}`
 
-      if branch
-        `cd #{repos.split('/').last}/`
-        `git checkout #{branch}`
-      end
+    if branch
+      `cd #{repos.split('/').last}/`
+      `git checkout #{branch}`
     end
+  end
 
-    def self.run(command)
-      `git #{command}`
+  def self.run(command)
+    `git #{command}`
+  end
+end
+
+# Run a command in git.
+#
+# ==== Examples
+#
+#   git :init
+#   git :add => "this.file that.rb"
+#   git :add => "onefile.rb", :rm => "badfile.cxx"
+#
+def git(command = {})
+  in_root do
+    if command.is_a?(Symbol)
+      log 'running', "git #{command}"
+      Git.run(command.to_s)
+    else
+      command.each do |command, options|
+        log 'running', "git #{command} #{options}"
+        Git.run("#{command} #{options}")
+      end
     end
   end
 end
