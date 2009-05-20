0aca941570414dec0bfb2cb73bb3a4017c6df60a
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-20 17:16:35 -0400

    cleanup cap module

--- lib/beet.rb
+++ lib/beet.rb
@@ -1,7 +1,9 @@
+# TODO similarly, figure out how to require things as needed by the template
 require 'beet/execution'
 require 'beet/file_system'
 require 'beet/interaction'
 require 'beet/logging'
+require 'beet/capistrano'
 require 'beet/rails'
 require 'beet/scm'
 require 'beet/executor'
--- lib/beet/capistrano.rb
+++ lib/beet/capistrano.rb
@@ -1,10 +1,14 @@
-# Just run the capify command in root
-#
-# ==== Example
-#
-#   capify!
-#
-def capify!
-  log 'capifying'
-  in_root { run('capify .', false) }
+module Beet
+  module Capistrano
+    # Just run the capify command in root
+    #
+    # ==== Example
+    #
+    #   capify!
+    #
+    def capify!
+      log 'capifying'
+      in_root { run('capify .', false) }
+    end
+  end
 end
