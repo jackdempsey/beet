e13f80eb28dbbd0ccf742275d4096caad0e0a2b8
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-20 15:05:28 -0400

    these templates are rails specific

---
+++ lib/beet/templates/rails/clean_files.rb
@@ -0,0 +1,4 @@
+run "rm README"
+run "rm public/index.html"
+run "rm public/favicon.ico"
+run "rm public/robots.txt"
---
+++ lib/beet/templates/rails/jquery.rb
@@ -0,0 +1,2 @@
+run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js > public/javascripts/jquery.js"
+run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
