88970f07737aa8b7b35dbba6e1ace25ab0b52775
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-19 21:42:48 -0400

    mixing needed methods and touch up handling of template

--- lib/beet/executor.rb
+++ lib/beet/executor.rb
@@ -1,18 +1,23 @@
+require 'open-uri'
 module Beet
   class Executor
+    include Beet::Execution
+    include Beet::FileSystem
+    include Beet::Interaction
+    include Beet::Logging
+
     attr_reader :root
     attr_writer :logger
+    attr_accessor :template
 
     def initialize(template, root = '') # :nodoc:
       @root = File.expand_path(File.directory?(root) ? root : File.join(Dir.pwd, root))
-      log 'applying', "template: #{template}"
-      load_template(template)
-      log 'applied', "#{template}"
+      @template = template
     end
 
-    def load_template(template)
+    def run
       begin
-        code = open(template).read
+        code = open(@template).read
         in_root { self.instance_eval(code) }
       rescue LoadError, Errno::ENOENT => e
         raise "The template [#{template}] could not be loaded. Error: #{e}"
