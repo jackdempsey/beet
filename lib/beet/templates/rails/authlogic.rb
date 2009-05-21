26d921ca2a21e6ddf0014d4ac5a6773c5f9ade7d
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-21 14:14:18 -0400

    fixup application controller definition

--- lib/beet/templates/rails/authlogic.rb
+++ lib/beet/templates/rails/authlogic.rb
@@ -73,23 +73,54 @@ route 'map.register "/register", :controller => "users", :action => "new"'
 route 'map.login "/login", :controller => "user_sessions", :action => "new"'
 route 'map.logout "/logout", :controller => "user_sessions", :action => "destroy"'
 
-file "app/controllers/application.rb" do
+file "app/controllers/application_controller.rb" do
   %{
-  class ApplicationController < ActionController::Base
-    filter_parameter_logging :password, :password_confirmation
-    helper_method :current_user_session, :current_user
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
 
-    private
-      def current_user_session
-        return @current_user_session if defined?(@current_user_session)
-        @current_user_session = UserSession.find
-      end
+class ApplicationController < ActionController::Base
+  helper :all
+  helper_method :current_user_session, :current_user
+  filter_parameter_logging :password, :password_confirmation
 
-      def current_user
-        return @current_user if defined?(@current_user)
-        @current_user = current_user_session && current_user_session.user
+  private
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.record
+    end
+
+    def require_user
+      unless current_user
+        store_location
+        flash[:notice] = "You must be logged in to access this page"
+        redirect_to new_user_session_url
+        return false
       end
-  end
+    end
+
+    def require_no_user
+      if current_user
+        store_location
+        flash[:notice] = "You must be logged out to access this page"
+        redirect_to account_url
+        return false
+      end
+    end
+
+    def store_location
+      session[:return_to] = request.request_uri
+    end
+
+    def redirect_back_or_default(default)
+      redirect_to(session[:return_to] || default)
+      session[:return_to] = nil
+    end
+end
 }
 end
 
