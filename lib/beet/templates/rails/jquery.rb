e799b9266b871df58a311bccfc74477c9ce44f86
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-24 21:39:59 -0400

    don't want prototype stuff cluttering things up

--- lib/beet/templates/rails/jquery.rb
+++ lib/beet/templates/rails/jquery.rb
@@ -1,2 +1,5 @@
+in_root { FileUtils.rm_r Dir.glob("public/javascripts/*.js") }
+
 run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js > public/javascripts/jquery.js"
 run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
+
