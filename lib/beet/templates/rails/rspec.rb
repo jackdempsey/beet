426f0e10d53133f0e17a0bc1fde8c6cf3a3862c3
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-29 03:16:11 -0400

    add in rspec rails recipe

---
+++ lib/beet/templates/rails/rspec.rb
@@ -0,0 +1,3 @@
+ruby script/plugin install git://github.com/dchelimsky/rspec.git -r 'refs/tags/1.2.6'
+ruby script/plugin install git://github.com/dchelimsky/rspec-rails.git -r 'refs/tags/1.2.6'
+ruby script/generate rspec
