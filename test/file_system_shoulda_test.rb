bb944f9d7ff212775667506a9a2e9da06c58a825
Jack Dempsey <jack.dempsey@gmail.com>
2009-05-30 00:40:48 -0400

    beginnings of the file system tests

---
+++ test/file_system_shoulda_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class FileSystemShouldaTest < Test::Unit::TestCase
+  context "#add_after" do
+    should "add the given text after the specified text" do
+      flunk "hey buddy, you should probably rename this file and start testing for real"
+    end
+    should "not add the given text if it already exists after the specified text" do
+      flunk "hey buddy, you should probably rename this file and start testing for real"
+    end
+  end
+end
