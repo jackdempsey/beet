659229f56f99153924c29b854e6320fe3f475efb
Jack Dempsey <jack.dempsey@gmail.com>
2009-06-04 02:02:10 -0400

    fixup project root detection tests

--- test/executor_test.rb
+++ test/executor_test.rb
@@ -2,14 +2,23 @@ require 'test_helper'
 
 class ExecutorShouldaTest < Test::Unit::TestCase
 
-  should "set project root to current directory if name given is not a folder" do
+  should "set project root to Dir.pwd + project_name in general case" do
     executor = Beet::Executor.new('foobar')
-    assert_equal Dir.pwd, executor.root
+    assert_equal Dir.pwd + "/foobar", executor.root
   end
 
-  should "set project root to directory name if project name looks like /Users/jack/something" do
+  should "set project root to project name if project name looks like /Users/jack/something" do
     executor = Beet::Executor.new('/Users/jack/foobar')
-    assert_equal '/Users/jack', executor.root
+    assert_equal '/Users/jack/foobar', executor.root
+  end
+
+  should "set project root to Dir.pwd if directory one level above is the same as the project name" do
+    FileUtils.mkdir_p 'foobar'
+    FileUtils.chdir 'foobar' do 
+      executor = Beet::Executor.new('foobar')
+      assert_equal Dir.pwd, executor.root
+    end
+    FileUtils.rm_rf 'foobar'
   end
 
 end
