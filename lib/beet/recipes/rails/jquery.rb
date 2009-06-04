29861f038ce35bd70cccab94c03e2b7c1881f4ea
Jack Dempsey <jack.dempsey@gmail.com>
2009-06-04 14:53:52 -0400

    add some useful ajax setting for jquery

--- lib/beet/recipes/rails/jquery.rb
+++ lib/beet/recipes/rails/jquery.rb
@@ -2,3 +2,10 @@ FileUtils.rm_r Dir.glob("public/javascripts/*.js")
 
 run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js > public/javascripts/jquery.js"
 run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
+
+# Thanks to Chris Wanstrath and Doug Ramsay. The below will make it so that Rails recognizes form requests
+file 'public/javascripts/application.js', <<-CODE
+jQuery.ajaxSetup({
+'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+});
+CODE
