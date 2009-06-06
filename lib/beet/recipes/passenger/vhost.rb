3fb7c263ccd1f2db631cf6d0b46e130adfbef28d
Jack Dempsey <jack.dempsey@gmail.com>
2009-06-05 22:33:52 -0400

    try to be smarter about vhost generation and placement

--- lib/beet/recipes/passenger/vhost.rb
+++ lib/beet/recipes/passenger/vhost.rb
@@ -1,16 +1,17 @@
-in_root do
-  file "#{project_name}.local.vhost.conf" do
-    %{
+file "#{project_name}.local.vhost.conf" do
+  %{
 <VirtualHost *:80>
-  ServerName #{project_name}.local
-  DocumentRoot "#{root}/public"
-  RackEnv development
-  <directory "#{root}/public">
-    Order allow,deny
-    Allow from all
-  </directory>
+ServerName #{project_name}.local
+DocumentRoot "#{root}/public"
+RackEnv development
+<directory "#{root}/public">
+  Order allow,deny
+  Allow from all
+</directory>
 </VirtualHost>
 }.strip
-  end
-  run "sudo mv ./#{project_name}.local.vhost.conf /etc/apache2/passenger_pane_vhosts"
 end
+default_to = "/etc/apache2/passenger_pane_vhosts"
+answer = ask "Write file to: [#{default_to} default]"
+filename = answer.empty? ? default_to : answer
+run "sudo mv ./#{project_name}.local.vhost.conf #{filename}"
