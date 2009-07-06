56062e88b0ce8a96753f05184e35f130374aa19b
Fares Donaldson <fares@Carols-Mac.local>
2009-07-06 15:27:22 +0100

    updating jquery

--- lib/beet/recipes/rails/jquery.rb
+++ lib/beet/recipes/rails/jquery.rb
@@ -1,6 +1,6 @@
 FileUtils.rm_r Dir.glob("public/javascripts/*.js")
 
-run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js > public/javascripts/jquery.js"
+run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js"
 run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
 
 # Thanks to Chris Wanstrath and Doug Ramsay. The below will make it so that Rails recognizes form requests
