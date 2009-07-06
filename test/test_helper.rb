23da8419c77b7d301d1cd9ce9d7a22a9085a83db
Fares Donaldson <fares@Carols-Mac.local>
2009-07-06 16:04:54 +0100

    changing chek to work on 1.8.6

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require 'shoulda'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'beet'
-if RUBY_VERSION <= '1.8'
+if RUBY_VERSION <= '1.8.6'
 require 'ruby-debug'
 end
 class Test::Unit::TestCase
