0b515c4cbf2f9ddd9baf3f1fc501b7e75e94c8a2
Jack Dempsey <jack.dempsey@gmail.com>
2009-11-13 01:56:28 -0500

    fixup devise strategy

--- lib/beet/recipes/rails/auth/devise.rb
+++ lib/beet/recipes/rails/auth/devise.rb
@@ -2,11 +2,11 @@ gem 'warden', :version => '~> 0.5.1'
 gem 'devise', :version => '~> 0.4.1'
 
 generate "devise_install"
-
 generate "devise User"
-
 generate "devise_views"
 
-add_after 'config/environments/development.rb', 'config.action_mailer.raise_delivery_errors', "config.action_mailer.default_url_options = { :host => 'localhost:3000' }"
+append_file 'config/environments/development.rb', "\nconfig.action_mailer.default_url_options = { :host => 'localhost:3000' }\n"
+append_file 'config/environment.rb', "\nDeviseMailer.sender = 'test@example.com'\n"
+add_after 'config/routes.rb', '# map.root :controller => "welcome"', "\nmap.root :controller => 'home'\n"
 
 rake "db:migrate"
