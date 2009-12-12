d999cbe587361de1aa1dd52fd7922f10162912e7
Jack Dempsey <jack.dempsey@gmail.com>
2009-12-11 20:23:20 -0500

    have devise create a nice home controller for you

--- lib/beet/recipes/rails/auth/devise.rb
+++ lib/beet/recipes/rails/auth/devise.rb
@@ -8,6 +8,17 @@ generate "devise_views"
 
 append_file 'config/environments/development.rb', "\nconfig.action_mailer.default_url_options = { :host => 'localhost:3000' }\n"
 append_file 'config/environment.rb', "\nDeviseMailer.sender = 'test@example.com'\n"
-add_after 'config/routes.rb', '# map.root :controller => "welcome"', "\nmap.root :controller => 'home'\n"
+add_after 'config/routes.rb', '# map.root :controller => "welcome"', "\n  map.root :controller => 'home'\n"
 
 rake "db:migrate"
+
+file 'app/controllers/home_controller.rb' do
+  %{
+class HomeController < ApplicationController
+  def index 
+    render :text => "Welcome!"
+  end
+end
+  }
+end
+
