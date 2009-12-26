61ebc4b6d3b158da900529a28736593155c20640
Jack Dempsey <jack.dempsey@gmail.com>
2009-12-26 00:16:35 -0500

    add in style and some other tweaks

--- lib/beet/recipes/rails/swfupload.rb
+++ lib/beet/recipes/rails/swfupload.rb
@@ -1,26 +1,94 @@
-base_url = 'http://code.google.com/p/swfupload/source/browse/swfupload/trunk/core/plugins'
-
-js_files = %w(swfupload.cookies swfupload.queue swfupload.speed swfupload.swfobject)
-
+# setup directories to hold files
 run 'mkdir public/javascripts/swfupload'
 run 'mkdir public/flash'
 run 'mkdir app/views/swfupload'
+run 'mkdir app/stylesheets'
 
+# download files
+base_url = 'http://code.google.com/p/swfupload/source/browse/swfupload/trunk/core/plugins'
+
+js_files = %w(swfupload.cookies swfupload.queue swfupload.speed swfupload.swfobject)
 
 js_files.each do |js_file|
   run "curl -L #{base_url}/#{js_file}.js > public/javascripts/swfupload/#{js_file}.js"
 end
 
-run "curl -L http://code.google.com/p/swfupload/source/browse/swfupload/trunk/core/Flash/swfupload.swf > public/javascripts/public/flash/swfupload.swf"
+run "curl -L http://code.google.com/p/swfupload/source/browse/swfupload/trunk/core/Flash/swfupload.swf > public/flash/swfupload.swf"
 
+file 'app/stylesheets/swfupload.sass', <<-FILE
+divFileProgressContainer
+  height: 75px
 
-file 'app/views/swfupload/_upload.html.haml', <<-FILE
+#divFileProgress
+
+  .progressWrapper
+    width: 100%
+    overflow: hidden
+
+  .progressContainer
+    margin: 5px
+    padding: 4px
+    border: solid 1px #E8E8E8
+    background-color: #F7F7F7
+    overflow: hidden
+    
+  .progressName
+    font-size: 8pt
+    font-weight: 700
+    color: #555
+    width: 100%
+    height: 14px
+    text-align: left
+    white-space: nowrap
+    overflow: hidden
+
+  .progressBarInProgress, .progressBarComplete, .progressBarError
+    font-size: 0
+    width: 0%
+    height: 2px
+    background-color: blue
+    margin-top: 2px
+    
+  .progressBarComplete
+    width: 100%
+    background-color: green
+    visibility: hidden
+    
+  .progressBarError
+    width: 100%
+    background-color: red
+    visibility: hidden
+
+  .progressBarStatus
+    margin-top: 2px
+    width: 337px
+    font-size: 7pt
+    font-family: Arial
+    text-align: left
+    white-space: nowrap
+      
+  a.progressCancel
+    font-size: 0
+    display: block
+    height: 14px
+    width: 14px
+    background-image: url(/images/swfupload/cancelbutton.gif)
+    background-repeat: no-repeat
+    background-position: -14px 0px
+    float: right
+
+  a.progressCancel:hover
+    background-position: 0px 0px
+FILE
+
+# create upload partial
+file 'app/views/swfupload/_upload.html.haml', %q{
 - content_for :head do
   = javascript_include_tag 'swfupload/swfupload', 'swfupload/swfupload.swfobject', 'swfupload/handlers'
   = stylesheet_link_tag %w(compiled/swfupload)
 - session_key_name = ActionController::Base.session_options[:key]
 
-- upload_url = swf_upload_account_admin_site_gallery_photo_album_photos_path(@account, @photo_album) 
+- upload_url = '/upload/url/here'
 - form_tag upload_url, :multipart => true do
   #swfupload_degraded_container
     %noscript= "You should have Javascript enabled for a nicer upload experience"
@@ -29,52 +97,9 @@ file 'app/views/swfupload/_upload.html.haml', <<-FILE
   #swfupload_container{ :style => "display: none" }
     %span#spanButtonPlaceholder
   #divFileProgressContainer
-FILE
 
-create_action = <<-FILE
-require 'mp3info'
-  mp3_info = Mp3Info.new(params[:Filedata].path)
 
-  song = Song.new
-  song.artist = mp3_info.tag.artist
-  song.title = mp3_info.tag.title
-  song.length_in_seconds = mp3_info.length.to_i
-
-  params[:Filedata].content_type = MIME::Types.type_for(params[:Filedata].original_filename).to_s
-  song.track = params[:Filedata]
-  song.save
-
-  render :text => [song.artist, song.title, song.convert_seconds_to_time].join(" - ")
-rescue Mp3InfoError => e
-  render :text => "File error"
-rescue Exception => e
-  render :text => e.message
-end 
-FILE
-
-model = <<-FILE
-class Song < ActiveRecord::Base
- 
-  has_attached_file :track,
-                    :path => ":rails_root/public/assets/:attachment/:id_partition/:id/:style/:basename.:extension",
-                    :url => "/assets/:attachment/:id_partition/:id/:style/:basename.:extension"
- 
-  validates_presence_of :title, :artist, :length_in_seconds
-  validates_attachment_presence :track
-  validates_attachment_content_type :track, :content_type => [ 'application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3' ]
-  validates_attachment_size :track, :less_than => 20.megabytes
- 
-  attr_accessible :title, :artist, :length_in_seconds
- 
-  def convert_seconds_to_time
-    total_minutes = length_in_seconds / 1.minutes
-    seconds_in_last_minute = length_in_seconds - total_minutes.minutes.seconds
-    "#{total_minutes}m #{seconds_in_last_minute}s"
-  end
-end
-FILE
-
-javascript_config = <<-FILE
+:javascript
   SWFUpload.onload = function() {
     var swf_settings = {
 
@@ -96,7 +121,9 @@ javascript_config = <<-FILE
       upload_url: "#{upload_url}",
       flash_url: '/flash/swfupload/swfupload.swf',
 
-      file_types: "*.jpg;*.jpeg;*.gif;*.png;",
+      //example below lets you scope to only allowing image uploads
+      //file_types: "*.jpg;*.jpeg;*.gif;*.png;",
+      file_types: "*",
       file_types_description: "pictures",
       file_size_limit: "20 MB",
 
@@ -122,7 +149,36 @@ javascript_config = <<-FILE
     }
     var swf_upload = new SWFUpload(swf_settings);
   };
+}
+
+create_action = <<-FILE
+def create
+  params[:Filedata].content_type = MIME::Types.type_for(params[:Filedata].original_filename).to_s
+  song = Song.new
+  # set song attributes, etc
+  song.save
+  render :text => "File information here that shows up as the result to the client"
+rescue Exception => e
+  render :text => e.message
+end 
 FILE
+
+
+model_file = <<-FILE
+class Song < ActiveRecord::Base
+ 
+  has_attached_file :track
+ 
+  validates_presence_of :title, :artist, :length_in_seconds
+  validates_attachment_presence :track
+  validates_attachment_content_type :track, :content_type => [ 'application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3' ]
+  validates_attachment_size :track, :less_than => 20.megabytes
+ 
+  attr_accessible :title, :artist, :length_in_seconds
+ 
+end
+FILE
+
 
 
 file 'config/initializers/session_store.rb', <<-FILE
@@ -145,3 +201,9 @@ end
 
 ActionController::Dispatcher.middleware.insert_before(ActionController::Base.session_store, FlashSessionCookieMiddleware, ActionController::Base.session_options[:key])
 FILE
+
+puts '=' * 80
+puts "\nYour controller's create action should look something like this:\n#{create_action}"
+
+puts '=' * 80
+puts "\nYou can create a model that looks like this:\n#{model_file}"
