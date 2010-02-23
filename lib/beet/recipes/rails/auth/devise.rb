c7d228d18c0174c60b9096f94566191327796732
Jack Dempsey <jack.dempsey@gmail.com>
2010-02-22 19:58:45 -0500

    update devise to user newer gems

--- lib/beet/recipes/rails/auth/devise.rb
+++ lib/beet/recipes/rails/auth/devise.rb
@@ -1,5 +1,5 @@
-gem 'warden', :version => '~> 0.6.5'
-gem 'devise', :version => '~> 0.7.1'
+gem 'warden', :version => '0.9.3'
+gem 'devise', :version => '1.0.2'
 
 rake 'gems:install'
 generate "devise_install"
@@ -7,7 +7,6 @@ generate "devise User"
 generate "devise_views"
 
 append_file 'config/environments/development.rb', "\nconfig.action_mailer.default_url_options = { :host => 'localhost:3000' }\n"
-append_file 'config/environment.rb', "\nDeviseMailer.sender = 'test@example.com'\n"
 add_after 'config/routes.rb', '# map.root :controller => "welcome"', "\n  map.root :controller => 'home'\n"
 
 rake "db:migrate"
