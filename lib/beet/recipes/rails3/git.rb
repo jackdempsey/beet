e4bdc53b34814f861510d2cddc5b4fe6f68487ad
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-25 12:05:44 -0400

    add rails3 dir for rails3 compatible recipes
    first recipe (dead simple) is a simple git init add and commit

---
+++ lib/beet/recipes/rails3/git.rb
@@ -0,0 +1,8 @@
+# .gitignore is now created for us by rails
+# only need to init git, add the current dir, and commit!
+
+git :init
+
+git :add => "."
+
+git :commit => "-m 'Initial commit from Beet. Enjoy.'"
