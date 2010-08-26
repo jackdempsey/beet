d05a9a8846044acf7842553146cf476108601aec
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-26 11:57:09 -0400

    cleanup cucumber recipe with new gem_group option

--- lib/beet/recipes/rails3/testing/cucumber.rb
+++ lib/beet/recipes/rails3/testing/cucumber.rb
@@ -1,11 +1,12 @@
-gem 'capybara', :group => :cucumber
-
-gem 'database_cleaner', :group => :cucumber
-gem 'cucumber-rails', :group => :cucumber
-gem 'cucumber', :group => :cucumber, :version => '0.7.3'
-gem 'rspec-rails', :group => :cucumber, :version => '~> 2.0.0.beta.19'
-gem 'spork', :group => :cucumber
-gem 'launchy', :group => :cucumber
+gem_group :cucumber do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'cucumber-rails'
+  gem 'cucumber', :version => '0.7.3'
+  gem 'rspec-rails', :version => '~> 2.0.0.beta.19'
+  gem 'spork'
+  gem 'launchy'
+end
 
 run 'bundle install'
 
