9f3c17bf97ac195f5978cfd66068008d775096ee
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-31 13:58:15 -0400

    much simpler jquery in rails 3 given indirect's jquery-rails gem

--- lib/beet/recipes/rails3/js/jquery.rb
+++ lib/beet/recipes/rails3/js/jquery.rb
@@ -1,9 +1,5 @@
-%w(application controls dragdrop effects prototype rails).each do |filename|
-  FileUtils.rm("public/javascripts/#{filename}.js")
-end
+gem "jquery-rails"
 
-run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.4.1.min.js > public/javascripts/jquery.js"
-run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
-run "curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js"
+run "bundle install"
 
-prepend_file 'public/javascripts/rails.js', %{jQuery.ajaxSetup({'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript");}})}
+generate "jquery:install --ui"
