46b41ee9c5b31ad4d462bb6b5e46fbad71ebcbe9
Jack Dempsey <jack.dempsey@gmail.com>
2010-09-08 16:45:22 -0400

    defer to AS local generators

--- lib/beet/recipes/rails3/admin_interface/active_scaffold.rb
+++ lib/beet/recipes/rails3/admin_interface/active_scaffold.rb
@@ -1,13 +1,10 @@
 # install plugins
 #
 plugin 'active_scaffold', :git => 'git://github.com/vhochstein/active_scaffold.git'
-plugin 'verification', :git => 'git://github.com/rails/verification.git'
-plugin 'render_component', :git => 'git://github.com/vhochstein/render_component.git'
-
-# configure as much as we can automatically
-add_after 'app/views/layouts/application.html.erb','  <%= csrf_meta_tag %>', '  <%= active_scaffold_includes %>'
 
 if yes?("Use jquery? (y/n):")
-  gsub_file 'vendor/plugins/active_scaffold/environment.rb', /#ActiveScaffold.js_framework = :jquery/, 'ActiveScaffold.js_framework = :jquery'
+  generate "active_scaffold_setup jquery"
+else
+  generate "active_scaffold_setup"
 end
 
